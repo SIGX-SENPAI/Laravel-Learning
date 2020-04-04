a91bc8a4c8a128967f241db2d452905c3bf132a7
SIGX-SENPAI <christian2011b@hotmail.com>
2020-04-04 00:42:08 -0500

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { resolve } from 'url';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Capitan America';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  heroe = {
+    nombre: 'logan',
+    clave: 'wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primer',
+      casa: 20
+    }
+  };
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
 }
